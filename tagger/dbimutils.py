4b9ce1fc38697e8a1c98a0008a1d848ef0fcdc47
corkborg <62742115+corkborg@users.noreply.github.com>
2024-05-25 14:22:59 +0900

    Remove dependency of opencv.

--- tagger/dbimutils.py
+++ tagger/dbimutils.py
@@ -1,8 +1,8 @@
 # DanBooru IMage Utility functions
 
-import cv2
 import numpy as np
-from PIL import Image
+import numpy.typing
+from PIL import Image, ImageOps
 
 def fill_transparent(image: Image.Image, color='WHITE'):
     image = image.convert('RGBA')
@@ -25,50 +25,55 @@ def resize(pic: Image.Image, size: int, keep_ratio=True) -> Image.Image:
 
     return pic.resize(target_size, resample=Image.Resampling.LANCZOS)
 
-def smart_imread(img, flag=cv2.IMREAD_UNCHANGED):
+def smart_imread(img):
     if img.endswith(".gif"):
-        img = Image.open(img)
-        img = img.convert("RGB")
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        with Image.open(img) as img_pil:
+            img_pil = img_pil.convert("RGB")
+            img_array = np.array(img_pil)
+            img = img_array[:, :, ::-1]  # Convert RGB to BGR
     else:
-        img = cv2.imread(img, flag)
+        with Image.open(img) as img_pil:
+            img = np.array(img_pil)
     return img
 
-
-def smart_24bit(img):
+def smart_24bit(img: np.ndarray) -> np.ndarray:
     if img.dtype is np.dtype(np.uint16):
         img = (img / 257).astype(np.uint8)
 
     if len(img.shape) == 2:
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img = np.stack([img, img, img], axis=-1)
     elif img.shape[2] == 4:
-        trans_mask = img[:, :, 3] == 0
-        img[trans_mask] = [255, 255, 255, 255]
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+        alpha_channel = img[:, :, 3]
+        trans_mask = alpha_channel == 0
+        img[trans_mask] = [255, 255, 255, 255]  # Fill transparent areas with white
+        img = img[:, :, :3]  # Drop the alpha channel
     return img
 
+def make_square(img: np.ndarray, target_size: int):
+    img_pil = Image.fromarray(img)
 
-def make_square(img, target_size):
-    old_size = img.shape[:2]
+    old_size = img_pil.size
     desired_size = max(old_size)
     desired_size = max(desired_size, target_size)
 
-    delta_w = desired_size - old_size[1]
-    delta_h = desired_size - old_size[0]
-    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
-    left, right = delta_w // 2, delta_w - (delta_w // 2)
+    delta_w = desired_size - old_size[0]
+    delta_h = desired_size - old_size[1]
+    pad_left, pad_right = delta_w // 2, delta_w - (delta_w // 2)
+    pad_top, pad_bottom = delta_h // 2, delta_h - (delta_h // 2)
 
-    color = [255, 255, 255]
-    new_im = cv2.copyMakeBorder(
-        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
-    )
-    return new_im
+    padding = (pad_left, pad_top, pad_right, pad_bottom)
+    img_square = ImageOps.expand(img_pil, padding, fill='white')
 
+    return np.array(img_square)
 
-def smart_resize(img, size):
-    # Assumes the image has already gone through make_square
-    if img.shape[0] > size:
-        img = cv2.resize(img, (size, size), interpolation=cv2.INTER_AREA)
-    elif img.shape[0] < size:
-        img = cv2.resize(img, (size, size), interpolation=cv2.INTER_CUBIC)
-    return img
+def smart_resize(img: np.ndarray, size):
+    img_pil = Image.fromarray(img)
+    if max(img_pil.size) > size:
+        img_resized = img_pil.resize((size, size), Image.LANCZOS)
+        return img_resized
+    elif max(img_pil.size) < size:
+        img_resized = img_pil.resize((size, size), Image.BICUBIC)
+    else:
+        img_resized = img_pil
+
+    return np.array(img_resized)
